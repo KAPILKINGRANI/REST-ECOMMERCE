fb5df94e59dffc06868f0abb6c14420e5f875db0
Kapil <kingranikapil3@gmail.com>
2024-07-02 19:58:46 +0530

    added general seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,5 +21,34 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        //if we are in production we wont truncate the table
+        if(App::environment() === 'production') exit();
+
+        //below code is only for development mode
+        //we truncate the table in development mode
+
+        $tables = DB::select('SHOW TABLES');
+        $key = "Tables_in_".env('DB_DATABASE');
+
+        /*
+         This disables foreign key checks in the database. It's necessary because truncating tables that have foreign key constraints would result in errors if the constraints are violated. Disabling these checks allows for truncating tables without worrying about foreign key relationships temporarily.
+         */
+        DB::statement("SET FOREIGN_KEY_CHECKS = 0");
+        foreach($tables as $table) {
+            if($table->key !== 'migrations') {
+                DB::table($table->$key)->truncate();
+            }
+        }
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
+            ProductSeeder::class,
+            TransactionSeeder::class
+        ]);
+
+        //Finally, this re-enables foreign key checks. This ensures that any future operations on the database will enforce foreign key constraints.
+        DB::statement("SET FOREIGN_KEY_CHECKS = 1");
+
     }
 }
