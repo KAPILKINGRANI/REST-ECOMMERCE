052e42f1a32423cb057028fb04b6c3e0a4acabed
Kapil <kingranikapil3@gmail.com>
2024-07-02 15:06:28 +0530

    added necessary routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('users',\App\Http\Controllers\User\UsersController::class);
+Route::apiResource('sellers',\App\Http\Controllers\Seller\SellersController::class);
+Route::apiResource('buyers',\App\Http\Controllers\Buyer\BuyersController::class);
+Route::apiResource('categories',\App\Http\Controllers\Category\CategoriesController::class);
+Route::apiResource('transactions',\App\Http\Controllers\Transaction\TransactionsController::class);
+Route::apiResource('products',\App\Http\Controllers\Product\ProductsController::class);
+
+
