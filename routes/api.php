8230109566a507eb2d36e37fe7655e8d7e2b9cb7
Kapil <kingranikapil3@gmail.com>
2024-07-05 15:49:25 +0530

    fetched all transactions from a product

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,4 @@ Route::apiResource('categories.sellers', \App\Http\Controllers\Category\Category
 Route::apiResource('sellers.transactions', \App\Http\Controllers\Seller\SellerTransactionsController::class)->only(['index']);
 Route::apiResource('sellers.categories', \App\Http\Controllers\Seller\SellerCategoriesController::class)->only(['index']);
 Route::apiResource('sellers.buyers', \App\Http\Controllers\Seller\SellerBuyersController::class)->only(['index']);
+Route::apiResource('products.transactions', \App\Http\Controllers\Product\ProductTransactionsController::class)->only(['index']);
